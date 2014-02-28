0452c6ddd36a87c751d0375bc2c469f57922900e
Seiya-Y <tyk50ps@gmail.com>
2014-02-28 22:19:51 +0900

    Create HelloWorld.cpp

---
+++ HelloWorld.cpp
@@ -0,0 +1,12 @@
+//HelloWorld.cpp
+
+#include<iostream>
+
+int main( void ){
+	int i=0;
+	
+	for( char message[20] = "Hello World !! ; ++i<13 ; std::cout<<message[i] );
+	std::cout<<endl;
+	
+	return 0;
+}
